204ed67d4f9efa65e83cf718f14e6203d1987ace
olsen-stettsen <ols17030@byui.edu>
2019-02-09 16:46:42 -0700

    updated week05

--- web/Week05/index.php
+++ web/Week05/index.php
@@ -35,7 +35,7 @@ $db = get_db();
             <option value="sally">Sally</option>
             <option value="all">All</option>
         </select> 
-        <input id="db" type="hidden" value="<?php echo get_db() ?>">
+        <input id="db" type="hidden" value="<?php $db ?>">
         <?php
         $statement = $db->prepare("SELECT user_name, post_text FROM temp_table");
         $statement->execute();
