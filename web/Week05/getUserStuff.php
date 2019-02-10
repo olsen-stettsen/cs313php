cdcdee160af56c70f9335445ea086b75e142563c
olsen-stettsen <ols17030@byui.edu>
2019-02-09 17:01:52 -0700

    updated week05

--- web/Week05/getUserStuff.php
+++ web/Week05/getUserStuff.php
@@ -1,4 +1,12 @@
 <?php
 $q = $_REQUEST["q"];
 echo "$q";
+$statement = $db->prepare("SELECT user_name, post_text FROM temp_table");
+$statement->execute();
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    $user = $row['user_name'];
+    $post = $row['post_text'];
+    echo "<p><i>$post</i> -$user";
+}
 ?> 
