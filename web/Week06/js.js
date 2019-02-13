f98f207a130a60a76bcfa3074b03d67790a907bf
olsen-stettsen <ols17030@byui.edu>
2019-02-12 21:48:15 -0700

    updated week06

--- web/Week06/js.js
+++ web/Week06/js.js
@@ -2,6 +2,7 @@
 function main(){
     //document.getElementById("topicselect").addEventListener("change", dbLookup);
     dbLookup(); //hardcoded
+    document.getElementById("btn_i").addEventListener("click", dbWrite);
 }
 function dbLookup(){
     //alert(document.getElementById("topicselect").value);
@@ -15,5 +16,24 @@ function dbLookup(){
         }
     };
     xmlhttp.open("GET", "getTopicStuff.php?q=" + topic, true); // '?' is the start and '&' is next var
+    xmlhttp.send();
+}
+function dbWrite(){
+    var book = document.getElementById("book_i").value;
+    var chapter = document.getElementById("chapter_i").value;
+    var verse = document.getElementById("verse_i").value;
+    var content = document.getElementById("content_i").value;
+    var topic = document.getElementById("topic_i").value;
+
+    alert("book=" + book + " chapter=" + chapter + " verse=" + verse + " content=" + content + " topic=" + topic);
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //alert(this.responseText);
+            //document.getElementById("output").innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("GET", "writeTopicStuff.php?book=" + book + "&chapter=" + chapter + "&verse=" + verse + "&content=" + content + "&topic=" + topic, true); // '?' is the start and '&' is next var
     xmlhttp.send();
 }
