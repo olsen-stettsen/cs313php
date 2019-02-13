5df38fa393487e39fdf5337219a8118adb522cd1
olsen-stettsen <ols17030@byui.edu>
2019-02-12 20:01:44 -0700

    updated week06

--- web/Week06/getTopicStuff.php
+++ web/Week06/getTopicStuff.php
@@ -17,7 +17,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     $verse = $row['verse'];
     $content = $row['content'];
  
-    $topics = $db->prepare('SELECT name FROM topic topic INNER JOIN topic_to_scripture_lookup lookup ON lookup.topic_id = topic.topic_id WHERE lookup.scripture_id = :scripture_id');
+    $topics = $db->prepare('SELECT name FROM topic topic INNER JOIN topic_to_scripture_lookup lookup ON lookup.topic_id = topic.topic_id WHERE lookup.scripture_id = topic.scripture_id');
 
     $topics->bindValue(':scripture_id', $row['scripture_id']);
         $topics->execute();
