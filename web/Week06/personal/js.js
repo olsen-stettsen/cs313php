7d293bad92354ed58c3496724b1fbebd57cd0a25
olsen-stettsen <ols17030@byui.edu>
2019-02-23 21:49:50 -0700

    updated week06

--- web/Week06/personal/js.js
+++ web/Week06/personal/js.js
@@ -39,5 +39,5 @@ function dbWrite(post){
     dbLookup();
 }
 function deletemessage(e){
-    alert(e.parentElement.parentElement.innerHTML);
+    alert(e.parentElement.parentElement.child[0].innerHTML);
 }
