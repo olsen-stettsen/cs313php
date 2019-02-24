0a94c7a8ab55aa7ad9943084cb114d018f081af8
olsen-stettsen <ols17030@byui.edu>
2019-02-24 15:44:00 -0700

    updated week06

--- web/Week06/personal/deletepost.php
+++ web/Week06/personal/deletepost.php
@@ -4,6 +4,6 @@ $db = get_db();
 $user = $_REQUEST["user"];
 $message = $_REQUEST["message"];
 
-$statement = $db->prepare("DELETE FROM temp_table WHERE post_text='$message' AND user_name='$user'");
+$statement = $db->prepare("DELETE FROM post_table WHERE post_text='$message' AND user_id=(SELECT user_id FROM account_table WHERE username='$user')");
 $statement->execute();    
 ?>
--- web/Week06/personal/writePostStuff.php
+++ web/Week06/personal/writePostStuff.php
@@ -5,6 +5,6 @@ $user = $_REQUEST["user"];
 $message = $_REQUEST["message"];
 
 $statement = $db->prepare("INSERT INTO post_table (user_id, post_text)
-                           VALUES ((SELECT user_id FROM account_table WHERE username='steve'), '$message')");
+                           VALUES ((SELECT user_id FROM account_table WHERE username='$user'), '$message')");
 $statement->execute();    
 ?>
