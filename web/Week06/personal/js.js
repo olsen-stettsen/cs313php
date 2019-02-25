723b2984b8f37f33cafe98a6cbb1236bdab1f193
olsen-stettsen <ols17030@byui.edu>
2019-02-24 19:59:40 -0700

    updated week06

--- web/Week06/personal/js.js
+++ web/Week06/personal/js.js
@@ -88,6 +88,6 @@ function replymessage(e){
 function optionvisible(){
     var postlist = document.getElementsByClassName("messageoptions");
     for (var i = 0; i < postlist.length; i++){
-        alert(postlist[i].parentElement.children[0].innerHTML = localStorage.getItem("user"));
+        alert(postlist[i].parentElement.children[0].innerHTML);
     }
 }
