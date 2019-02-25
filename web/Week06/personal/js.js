a3530e61d5118e1ebb74a449e7f5541f3ada00b9
olsen-stettsen <ols17030@byui.edu>
2019-02-24 19:39:40 -0700

    updated week06

--- web/Week06/personal/js.js
+++ web/Week06/personal/js.js
@@ -1,5 +1,5 @@
 function main(){
-    document.getElementById("userselect").addEventListener("change", dbLookup);
+    //document.getElementById("userselect").addEventListener("change", dbLookup);
     document.getElementById("mkpostbtn").addEventListener("click", makepost);
     getUser();
     dbLookup();
