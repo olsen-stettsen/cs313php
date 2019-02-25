fd187ddc3c375182c4a375212d6bda016d5d8f25
olsen-stettsen <ols17030@byui.edu>
2019-02-24 20:51:13 -0700

    updated week06

--- web/Week06/personal/writeuser.php
+++ web/Week06/personal/writeuser.php
@@ -3,6 +3,6 @@ require "getDB.php";
 $db = get_db();
 $user = $_REQUEST["user"];
 
-$statement = $db->prepare("INSERT INTO account_table (account_table) VALUES ('$user')");
+$statement = $db->prepare("INSERT INTO account_table (username) VALUES ('$user')");
 $statement->execute();    
 ?>
