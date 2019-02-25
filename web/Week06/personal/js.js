9e3e84583d16c9c96e26ef970a433c0d28636d85
olsen-stettsen <ols17030@byui.edu>
2019-02-24 20:38:06 -0700

    updated week06

--- web/Week06/personal/js.js
+++ web/Week06/personal/js.js
@@ -19,7 +19,7 @@ function userLookup(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            //alert(this.responseText);
+            alert(this.responseText);
             ex = this.responseText;
         }
     };
