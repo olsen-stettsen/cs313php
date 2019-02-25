5e333a584568b6b47f51f24ff66f2418a4f590a8
olsen-stettsen <ols17030@byui.edu>
2019-02-24 20:15:57 -0700

    updated week06

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,8 +1,8 @@
 CREATE TABLE account_table (
     user_id         serial          PRIMARY KEY,
     username        VARCHAR (255)   UNIQUE NOT NULL,
-    user_password   VARCHAR (255)   NOT NULL,
-    email           VARCHAR (255)   NOT NULL,
+    user_password   VARCHAR (255),
+    email           VARCHAR (255),
     created_on      TIMESTAMP,
     last_login      TIMESTAMP
 );
@@ -16,7 +16,7 @@ CREATE TABLE reply_table (
     reply_id        serial          PRIMARY KEY,
     user_id         int             NOT NULL REFERENCES account_table(user_id),
     post_id         int             NOT NULL REFERENCES post_table(post_id),
-    repley_text     VARCHAR (500)   NOT NULL,
+    reply_text     VARCHAR (500)   NOT NULL,
     media_link      VARCHAR(500) 
 );
 INSERT INTO account_table (username, user_password, email) VALUES ('steve', '1234', 'steve@bob.com');
--- web/Week06/personal/replypost.php
+++ web/Week06/personal/replypost.php
@@ -5,7 +5,7 @@ $user = $_REQUEST["user"];
 $message = $_REQUEST["message"];
 $reply = $_REQUEST["reply"];
 
-$statement = $db->prepare("INSERT INTO reply_table (user_id, post_id, repley_text)
+$statement = $db->prepare("INSERT INTO reply_table (user_id, post_id, reply_text)
                            VALUES ((SELECT user_id FROM account_table WHERE username='$user'), (SELECT user_id FROM post_table WHERE post_text='$message'), '$reply')");
 $statement->execute();    
 ?>
