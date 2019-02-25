cd8f4f969c5570c5f25c828949555fb7efc8d3c2
olsen-stettsen <ols17030@byui.edu>
2019-02-24 18:37:04 -0700

    updated week06

--- web/Week06/personal/updatepost.php
+++ web/Week06/personal/updatepost.php
@@ -3,7 +3,9 @@ require "getDB.php";
 $db = get_db();
 $user = $_REQUEST["user"];
 $message = $_REQUEST["message"];
-$newmessage = $_REQUEST["newmessage"]
-echo $newmessage
- 
+$newmessage = $_REQUEST["newmessage"];
+echo $newmessage;
+
+$statement = $db->prepare("UPDATE post_table SET post_text='$newmessage' WHERE post_text='$message' AND user_id=(SELECT user_id FROM account_table WHERE username='$user')");
+$statement->execute();    
 ?>
