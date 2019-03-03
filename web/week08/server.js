06955debd9384bfaed6dbce433884105090e0307
olsen-stettsen <ols17030@byui.edu>
2019-03-02 23:54:48 -0700

    updated week06

--- web/week08/server.js
+++ web/week08/server.js
@@ -1,22 +1,25 @@
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = 8888;
 function onRequest(req, res){
   console.log("request");
   if (req.url == '/home'){
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('../index.html', (err, html) =>{
-      if(err){
-        console.log("no html");
-      }
-      res.write(html);
-    });
+    //res.setHeader('Content-Type', 'text/html');
     //res.write("<h1>Welcome to the Home Page</h1>");
   }
   else if (req.url == '/getData'){
     res.setHeader('Content-Type', 'application/json');
     res.write("{'name':'Br. Burton','class':'cs313'}");
+  }
+  else if(req.url == './style.css'){
+    const fs = require('fs');
+    fs.readFile('../style.css', (err, css) => {
+      if(err){
+        console.log("no css");
+      }
+      res.setHeader('Content-Type', 'text/css');
+      res.write(css);
+    });
   }
   else{
     res.setHeader('Content-Type', 'text/html');
@@ -24,14 +27,26 @@ function onRequest(req, res){
     res.statusCode = 404;
   }
 }
-const server = http.createServer(onRequest);
+//const server = http.createServer(onRequest);
+//server.listen(port);
+
+const fs = require('fs');
+fs.readFile('../index.html', function (err, html) {
+  if (err) {
+      throw err; 
+  }       
+  http.createServer(function(request, response) {  
+      response.writeHeader(200, {"Content-Type": "text/html"});  
+      response.write(html);  
+      response.end();
+  }).listen(8888);
+});
 /*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });*/
-server.listen(port);
 /*
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
