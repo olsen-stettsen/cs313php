b70e907427870aa31bf54400c8729e174f63744c
olsen-stettsen <ols17030@byui.edu>
2019-03-02 22:23:44 -0700

    updated week06

---
+++ web/week08/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8888;
+function onRequest(req, res){
+  console.log("request");
+  if (req.url == '/home'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<h1>Welcome to the Home Page</h1>");
+  }
+  else if (req.url == '/getData'){
+    res.setHeader('Content-Type', 'application/json');
+    res.write("{'name':'Br. Burton','class':'cs313'}");
+  }
+  else{
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<h1>Page Not Found</h1>");
+    res.statusCode = 404;
+  }
+}
+const server = http.createServer(onRequest);
+/*
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});*/
+server.listen(port);
+/*
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  onRequest();
+});*/
