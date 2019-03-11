d27740730b9b9e284ea553529474a6cf5d3a2a3c
olsen-stettsen <ols17030@byui.edu>
2019-03-11 00:55:08 -0600

    updated week06

--- web/week09/index.js
+++ web/week09/index.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.use(express.static("public"));
-app.get("/", function(res, res){
+function calculateRate(type, amount){
+    var r_amount;
+    switch(type) {
+        case 'flat':
+          r_amount = 12.53
+          break;
+        case 'premium':
+          r_amount = amount * 2;
+          break;
+        case 'platinum':
+          r_amount = amount * 1.6;
+          break;
+        case 'economic':
+          r_amount = amount * 1.15;
+          break;
+      } 
+      return r_amount;
+}
+
+app.use(express.static("public")); // serve files in this folder
+app.post("/getRate", function(req, res){
+    var inputContent = req.body.inputF;
+    var inputmethod = req.body.method;
+    console.log(inputContent);
+    console.log(inputmethod);
+    console.log(calculateRate(inputmethod, inputContent));
+    console.log("page2 requested");
+    //res.write("This page2");
+    //res.end();
+    res.render(path.join(__dirname+'/page2.ejs'), {key: inputContent, method: inputmethod, price: calculateRate(inputmethod, inputContent).toFixed(2)});
+});
+app.get("/", function(req, res){
     console.log("Request for root");
     res.sendFile(path.join(__dirname+'/index.html'));
 });
